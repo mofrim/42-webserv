e5324b8627055ec6a75cabd5b4f9a1de325707b0
mofrim <mofrim@posteo.de>
2025-11-24 19:58:30 +0100

    heavy wip

--- include/Webserv.hpp
+++ include/Webserv.hpp
@@ -6,9 +6,17 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 12:35:29 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/11/21 07:29:02 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/11/21 14:51:56 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <stdexcept>
+
+class Logger {
+	public:
+		void logMsg(const std::string& msg) { std::cout << msg << std::endl; };
+};
 
 class Webserv {
 	private:
@@ -19,5 +27,12 @@ class Webserv {
 		Webserv& operator=(const Webserv& other);
 		~Webserv();
 
-		// other funky funx
+		// defining exception runtime_error here because many errors we will
+		// encounter here will be produced during runtime
+		class MyException: public std::runtime_error {
+			public:
+				MyException(const std::string& msg);
+		};
+
+		class logger: public Logger {};
 };
