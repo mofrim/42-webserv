b14a73d6e3116973a8e7bac0cce1775a18331ad6
mofrim <mofrim@posteo.de>
2025-11-26 13:45:06 +0100

    work on default cfg

--- src/Webserv.cpp
+++ src/Webserv.cpp
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 12:36:43 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/11/26 10:12:39 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/11/26 10:46:59 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 // FIXME: remove cause sleep is not an allowed function!
 #include <unistd.h>
+
+// for memset
+#include <cstring>
 
 Webserv::Webserv(): _shutdown_server(false) {}
 
@@ -61,9 +64,30 @@ void Webserv::run(const Config& cfg)
 void Webserv::_setupServers()
 {
 
-	if (_cfg.isDefaultCfg())
+	if (_cfg.isDefaultCfg()) {
+		_initDefaultCfg();
 		_setupOneServer(_cfg.getCfgs()[0]);
+	}
 }
 
 // NEXT: implement a first socket setup for only the default Server
 void Webserv::_setupOneServer(const ServerCfg& cfg) { cfg.printCfg(); }
+
+void Webserv::_initDefaultCfg()
+{
+	ServerCfg		dcfg = _cfg.getCfgs()[0];
+	sockaddr_in srv_addr;
+
+	dcfg.setServerName(DEFAULT_SRV_NAME);
+	dcfg.setPort(DEFAULT_PORT);
+	dcfg.setRoot("./www");
+
+	memset(&srv_addr, 0, sizeof(srv_addr));
+	srv_addr.sin_family			 = AF_INET;
+	srv_addr.sin_addr.s_addr = INADDR_ANY;
+	srv_addr.sin_port				 = htons(DEFAULT_PORT);
+	dcfg.setServerAddr(srv_addr);
+	dcfg.setHost(INADDR_LOOPBACK);
+
+	_cfg.setDefaultCfg(dcfg);
+}
