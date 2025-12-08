0e5558d2b3b275301884b93997f7d9e424c93cab
mofrim <mofrim@posteo.de>
2025-12-08 13:25:10 +0100

    also delete other generated test files

--- tests/test-main.cpp
+++
@@ -1,9 +0,0 @@
-#include "test-utils.hpp"
-#include "tests.hpp"
-int main()
-{
-	print_test_file_header("test_example1");
-	test_example1();
-	print_test_file_header("test_example2");
-	test_example2();
-}
