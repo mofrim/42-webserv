881179f182646673e18b15b92020c9db007f6b9f
mofrim <mofrim@posteo.de>
2025-12-08 13:26:12 +0100

    tests: rename test & update mkfile base

--- tests/cpp_tests/test_server_init.cpp
+++ tests/cpp_tests/test_server_init.cpp
@@ -1,15 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_example1.cpp                                  :+:      :+:    :+:   */
+/*   test_server_init.cpp                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 11:40:47 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/12/08 11:41:43 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/12/08 13:21:20 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../test-utils.hpp"
+#include "Server.hpp"
+#include "ServerCfg.hpp"
 
-void test_example1() { print_test_section_header("example test #1"); }
+// TODO: implement this
+void test_server_init()
+{
+	print_test_section_header("Server init test");
+	ServerCfg cfg;
+	Server		srv(cfg);
+}
