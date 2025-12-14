a7ed341425e9d9794ef728ce778cd63d3476802f
mofrim <mofrim@posteo.de>
2025-12-14 20:47:44 +0100

    remove signal confusion code

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 12:37:25 by fmaurer           #+#    #+#             */
-/*   Updated: 2025/12/12 16:45:23 by fmaurer          ###   ########.fr       */
+/*   Updated: 2025/12/14 20:47:10 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,6 @@ void shutdownHandler(int signum)
 	}
 }
 
-// int signal_setup(void (*sig_handler)(int))
-// {
-// 	struct sigaction sig_act;
-//
-// 	memset(&sig_act, 0, sizeof(struct sigaction));
-// 	sig_act.sa_handler = sig_handler;
-// 	sig_act.sa_flags	 = 0;
-// 	sigemptyset(&sig_act.sa_mask);
-// 	if (sigaction(SIGINT, &sig_act, NULL) == -1) {
-// 		Logger::log_err("setting signal handler failed");
-// 		return (-1);
-// 	}
-// 	if (sigaction(SIGTERM, &sig_act, NULL) == -1) {
-// 		Logger::log_err("setting signal handler failed");
-// 		return (-1);
-// 	}
-// 	return (0);
-// }
-
 int main(int ac, char **av)
 {
 	Config	cfg;
@@ -55,11 +36,6 @@ int main(int ac, char **av)
 		Logger::log_err("setting signal handler failed");
 		return (1);
 	}
-
-	// if (signal_setup(shutdownHandler) != 0) {
-	// 	Logger::log_err("setting signal handler failed");
-	// 	return (1);
-	// }
 
 	if (ac == 1)
 		webserv.run(cfg);
