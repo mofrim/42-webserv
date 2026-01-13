28ef30c56dd274223144398cc2e0414e44909d3f
mofrim <mofrim@posteo.de>
2026-01-13 15:15:57 +0100

    further tests tuning

--- tests/cpp_tests/test_socket.cpp
+++ tests/cpp_tests/test_socket.cpp
@@ -1,23 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_socket.cpp                                  :+:      :+:    :+:   */
+/*   test_socket.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 13:06:35 by fmaurer           #+#    #+#             */
-/*   Updated: 2026/01/13 13:44:14 by fmaurer          ###   ########.fr       */
+/*   Updated: 2026/01/13 15:12:39 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../test-utils.hpp"
+#include "Socket.hpp"
 
 #include <iostream>
 #include <stdexcept>
 
 // test logic goes here, for any failure return (-1) or throw exception.
 // otherwise -> success
-int _test_socket() { return (0); }
+int _test_socket()
+{
+	Socket sock0("localhost", 1234);
+	sock0.print_addrllist();
+	Socket sock1("jeanluc", 1234);
+	sock1.print_addrllist();
+	Socket sock2("12312.a", 1234);
+	sock2.print_addrllist();
+
+	return (0);
+}
 
 void test_socket()
 {
@@ -26,11 +37,10 @@ void test_socket()
 	try {
 		ret = _test_socket();
 	} catch (const std::exception& e) {
-		print_test_fail(
-				">> Test \"test_socket\" failed with following exception:\n"
+		print_test_fail("Test \"test_socket\" failed with following exception:\n"
 				+ std::string(e.what()));
 	}
 	if (ret == -1) {
-		print_test_fail(">> Test \"test_socket\" failed with -1");
+		print_test_fail("Test \"test_socket\" failed with -1");
 	}
 }
