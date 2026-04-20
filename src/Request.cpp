74f3d66e47f1a2b3471f14a5c537392c1dee149b
mofrim <mofrim@posteo.de>
2026-04-20 18:22:32 +0200

    oh man! a lot to do in Request class

--- src/Request.cpp
+++ src/Request.cpp
@@ -6,10 +6,11 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 23:39:57 by fmaurer           #+#    #+#             */
-/*   Updated: 2026/04/18 15:52:51 by fmaurer          ###   ########.fr       */
+/*   Updated: 2026/04/20 18:20:46 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ErrPages.hpp"
 #include "Logger.hpp"
 #include "Request.hpp"
 
@@ -56,30 +57,24 @@ Request::Request(const VServerCfg *scfg, const std::string& reqstr)
 // TODO: a. lot. of. work. to. be. done. in. here!
 // TODO: in Repsonse class: construct Response string by appending "\r\n" aka
 // CRLF to each line of the response string.
+// TODO: automate response body length calculation
 //
 // Important resource:
 //
 // 	https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
 void Request::_parseRequest()
 {
-
   if (!_isTerminatedReq()) {
     _statusCode = 400;
     _response =
         "HTTP/1.1 400 Bad Request\r\n"
-        "Server: nginx/1.28.0\r\n"
+        "Server: m0fr1m-webserv\r\n"
         "Date: Mon, 05 Jan 2026 07:08:57 GMT\r\n"
         "Content-Type: text/html\r\n"
         "Content-Length: 157\r\n"
         "Connection: close\r\n"
-        "\r\n"
-        "<html>\r\n"
-        "<head><title>400 Bad Request</title></head>\r\n"
-        "<body>\r\n"
-        "<center><h1>400 Bad Request</h1></center>\r\n"
-        "<hr><center>nginx/1.28.0</center>\r\n"
-        "</body>\r\n"
-        "</html>\r\n";
+        "\r\n" +
+        ErrPages::getDefaultErrPage(400);
   }
   else
     _response =
