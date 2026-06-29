0274e3f369253cf64b8c555b03fa043c82e6fc19
fmaurer <fmaurer@school>
2026-06-29 14:24:17 +0200

    make 42tester wrapper a bit more flexible

--- tests/run_42tester.sh
+++ tests/run_42tester.sh
@@ -18,8 +18,8 @@ if [[ -n "$(grep 'jeanluc' <<< "$HOSTNAME")" ]]; then
 	sed -i 'c/mofrim/frido/g' ./cfgs/42tester.wsrv
 	sed -i 's/mofrim/frido/g' ./www/42tester/wrapper.sh
 elif [[ -n "$(grep 'wolfsburg' <<< "$HOSTNAME")" ]]; then
-	sed -i "/wrapper.sh/c cgi bla\:$PWD\/www\/42tester\/cgi_tester!" ./cfgs/42tester.wsrv
-	sed -i "/wrapper.sh/c cgi bla\:$PWD\/www\/42tester\/cgi_tester!" ./tests/_cfgs/42tester.wsrv
+	sed -i "/^[^#].*wrapper.sh/c cgi bla\:$PWD\/www\/42tester\/cgi_tester!" ./cfgs/42tester.wsrv
+	sed -i "/^[^#].*wrapper.sh/c cgi bla\:$PWD\/www\/42tester\/cgi_tester!" ./tests/_cfgs/42tester.wsrv
 else
 	sed -i 's/frido/mofrim/g' ./cfgs/42tester.wsrv
 	sed -i 's/frido/mofrim/g' ./www/42tester/wrapper.sh
